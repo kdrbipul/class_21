c9b7414c8a90df3d5dcaaff938b74758d42d7939
kdrbipul <kdrbipul@gmail.com>
2023-08-05 01:28:03 +0600

    last commit

--- main.js
+++ main.js
@@ -18,3 +18,18 @@
 //     console.log(p.innerText);
 // }
 
+// const secondTitle=document.querySelector(".second_title");
+// console.log(secondTitle);
+// const secondTitle=document.getElementsByClassName("second_title");
+// console.log(secondTitle);
+
+
+// const lastTitle=document.getElementById("Last_title");
+// console.log(lastTitle);
+
+const blogs=document.getElementsByClassName('blog');
+// console.log(blogs);
+
+for(const blog of blogs){
+    console.log(blog);
+}
