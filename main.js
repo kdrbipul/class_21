5b339c49248827347de8ad755076b4e3832126b7
kdrbipul <kdrbipul@gmail.com>
2023-08-05 01:14:52 +0600

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-alert()
+// console.log(document);
+
+// const student={name: "tufan", roll: 1, className:"ten"};
+// console.log(student.name);
+
+
+// console.log(typeof document);
+
+// const title=(document.getElementsByTagName('h3'));
+
+// for (const h3 of title){
+//     console.log(h3.innerText);
+// }
+
+// const paragraph=document.getElementsByTagName('p');
+// console.log(paragraph);
+// for(const p of paragraph){
+//     console.log(p.innerText);
+// }
+
